b5909900ec52841fac9ab0fecb3f7b942b3f6f66
Arundathi Shaji Shanthini <arundathisanthini@gmail.com>
2020-02-09 18:28:24 +0000

    Added calculations of heuristics for A star

--- comp0037_planner_controller/src/comp0037_planner_controller/aStar_planner.py
+++ comp0037_planner_controller/src/comp0037_planner_controller/aStar_planner.py
@@ -14,11 +14,29 @@ class AStarPlanner(CellBasedForwardSearch):
         with open("performance_metrics.txt", "a") as f:
             f.write('A* Algorithm (Heuristic: {}): \n'.format(heuristic))
 
-    
+    def calc_heuristics(self, cell, parentCell,heuristics):
+        if(heuristics='constant'):
+            return 5
+
+        elif(heuristics='euclidean'):
+            del_x = cell.coords[0] - self.goal.coords[0]
+            del_y = cell.coords[1] - self.goal.coords[1]
+            return sqrt(pow(del_x,2)+pow(del_y,2))
+            
+        elif(heuristics='octile'):
+            x_diff = abs(cell.coords[0]-self.goal.coords[0])
+            y_diff = abs(cell.coords[1]-self.goal.coords[1])
+            return max(x_diff,y_diff) + (sqrt(2)-1)*min(x_diff,y_diff)
+
+        elif(heuristics='manhattan'):
+            return abs(cell.coords[0]-self.goal.coords[0]) + abs(cell.coords[1]-self.goal.coords[1])
+
+        else:
+            return 0
 
     # Simply put on the end of the queue
     def pushCellOntoQueue(self, cell):
-        cost = cell.path_cost + self.computeLStageAdditiveCost(parentCell, cell)
+        cost = cell.path_cost + self.computeLStageAdditiveCost(parentCell, cell) + calc_heuristics()
         self.aStarQueue.put((dist, cell.parent , cell))
 
     # Check the queue size is zero
